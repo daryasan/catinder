48864c49a8ffc57e075e567b53ba9f21d953e1b6
Darya Sudakova <darya_smile17@mail.ru>
2025-05-16 23:05:43 +0300

    list of liked cats

--- lib/screens/liked_cats_screen.dart
+++ lib/screens/liked_cats_screen.dart
@@ -1,14 +1,30 @@
+import 'package:catinder/model/liked_cat.dart';
 import 'package:catinder/navigation/routes.dart';
 import 'package:catinder/state/tinder_notifier.dart';
+import 'package:catinder/widget/cat_item.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class LikedCatsScreen extends StatelessWidget {
+class LikedCatsScreen extends StatefulWidget {
   const LikedCatsScreen({super.key});
+
+  @override
+  State<LikedCatsScreen> createState() => _LikedCatsScreenState();
+}
+
+class _LikedCatsScreenState extends State<LikedCatsScreen> {
+  String selectedBreed = "All";
 
   @override
   Widget build(BuildContext context) {
     TinderNotifier notifier = Provider.of<TinderNotifier>(context);
+
+    List<String> breeds = ["All", ...notifier.likedCats.map((cat) => cat.cat.breed).toSet()];
+
+    List<LikedCat> filteredCats = selectedBreed == "All"
+        ? notifier.likedCats
+        : notifier.likedCats.where((cat) => cat.cat.breed == selectedBreed).toList();
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
@@ -19,7 +35,7 @@ class LikedCatsScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
         child: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(30),
@@ -28,31 +44,31 @@ class LikedCatsScreen extends StatelessWidget {
           ),
           child: Column(
             children: [
-              Expanded(
-                child: notifier.likedCats.isEmpty
-                    ? Center(child: Text("No liked cats yet!"))
-                    : ListView.builder(
-                  itemCount: notifier.likedCats.length,
-                  itemBuilder: (context, index) {
-                    final likedCat = notifier.likedCats[index];
-                    return Card(
-                      child: ListTile(
-                        onTap: () async {
-                          await Navigator.of(context).pushNamed(
-                            RouteNames.catDetails,
-                            arguments: [notifier.currentCat],
-                          );
-                        },
-                        leading: Image.network(likedCat.cat.image),
-                        title: Text(likedCat.cat.breed),
-                        subtitle: Text('Liked on: ${likedCat.likedAt}'),
-                        trailing: IconButton(
-                          icon: Icon(Icons.delete, color: Colors.red),
-                          onPressed: () =>
-                              notifier.removeLikedCat(likedCat),
-                        ),
-                      ),
+              Padding(
+                padding: const EdgeInsets.all(40),
+                child: DropdownButton<String>(
+                  value: selectedBreed,
+                  items: breeds.map((breed) {
+                    return DropdownMenuItem(
+                      value: breed,
+                      child: Text(breed),
                     );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedBreed = value!;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: filteredCats.isEmpty
+                    ? const Center(child: Text("No liked cats yet!"))
+                    : ListView.builder(
+                  itemCount: filteredCats.length,
+                  itemBuilder: (context, index) {
+                    final likedCat = filteredCats[index];
+                    return CatItem(cat: likedCat);
                   },
                 ),
               ),
---
+++ lib/widget/cat_item.dart
@@ -0,0 +1,95 @@
+import 'package:catinder/model/liked_cat.dart';
+import 'package:catinder/navigation/routes.dart';
+import 'package:catinder/state/tinder_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+class CatItem extends StatefulWidget {
+  final LikedCat cat;
+
+  const CatItem({super.key, required this.cat});
+
+  @override
+  State<CatItem> createState() => _CatItemState();
+}
+
+class _CatItemState extends State<CatItem> {
+  @override
+  Widget build(BuildContext context) {
+    final TinderNotifier notifier = Provider.of<TinderNotifier>(context);
+
+    final String formattedDate = DateFormat(
+      'dd MMMM HH:mm',
+    ).format(widget.cat.likedAt);
+
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: Card(
+        child: ListTile(
+          onTap: () async {
+            await Navigator.of(context).pushNamed(
+              RouteNames.catDetails,
+              arguments: [notifier.currentCat],
+            );
+          },
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: SizedBox(
+              width: 80,
+              height: 80,
+              child: Image.network(
+                widget.cat.cat.image,
+                fit: BoxFit.cover,
+                width: 80,
+                height: 80,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) {
+                    return child;
+                  }
+                  return Center(
+                    child: SizedBox(
+                      width: 80,
+                      height: 80,
+                      child: Center(child: const CircularProgressIndicator()),
+                    ),
+                  );
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    if (mounted) {
+                      showDialog(
+                        context: context,
+                        builder:
+                            (context) => AlertDialog(
+                              title: const Text("Ops, no internet!"),
+                              content: const Text(
+                                "You have a bad connection. Please, try again later!",
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () => Navigator.of(context).pop(),
+                                  child: const Text("OK"),
+                                ),
+                              ],
+                            ),
+                      );
+                    }
+                  });
+                  return const Icon(Icons.error, size: 50, color: Colors.black);
+                },
+              ),
+            ),
+          ),
+          title: Text(widget.cat.cat.breed),
+          subtitle: Text('Liked on: $formattedDate'),
+          trailing: IconButton(
+            icon: SvgPicture.asset("assets/images/delete_icon.svg"),
+            onPressed: () => notifier.removeLikedCat(widget.cat),
+          ),
+        ),
+      ),
+    );
+  }
+}
